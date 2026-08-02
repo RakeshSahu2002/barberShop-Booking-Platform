688ab556ab9c5831df8bca886e9d0e9059bc678b
RakeshSahu2002 <rakeshgolhani8461@gmail.com>
2026-08-02 18:21:48 +0530

    Test auth route

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Auth route working"
+  });
+});
+
 const { register, login, getProfile } = require("../controllers/auth.controller");
 const { verifyToken } = require("../middleware/auth");
 
