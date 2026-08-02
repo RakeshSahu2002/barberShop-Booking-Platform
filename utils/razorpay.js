8ac3a34df427cccc25f575f697abfc4ceff24bc2
RakeshSahu2002 <rakeshgolhani8461@gmail.com>
2026-08-02 15:54:02 +0530

    Debug razorpay environment

--- utils/razorpay.js
+++ utils/razorpay.js
@@ -1,9 +1,24 @@
 const Razorpay = require("razorpay");
 require("dotenv").config();
 
+console.log(
+  "RAZORPAY_KEY_ID:",
+  process.env.RAZORPAY_KEY_ID
+);
+
+console.log(
+  "RAZORPAY_SECRET:",
+  process.env.RAZORPAY_KEY_SECRET ? "Loaded" : "Missing"
+);
+
+
 const razorpayInstance = new Razorpay({
+
   key_id: process.env.RAZORPAY_KEY_ID,
+
   key_secret: process.env.RAZORPAY_KEY_SECRET,
+
 });
+
 
 module.exports = razorpayInstance;
